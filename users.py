4fc35b82752abaa49fb26c4b37c08842b9f82b63
mohaiminul69 <mohaiminul69@gmail.com>
2024-10-25 08:28:19 +0600

    all in same file.

--- users.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC
-
-
-class User(ABC):
-    def __init__(self, name, email, address, password):
-        self.name = name
-        self.email = email
-        self.address = address
-        self.password = password
